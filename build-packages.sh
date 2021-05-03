0ba756b773af6cc132b8e66d3ec15ac6a83b1c3f
Pawel Soja <kernel32.pl@gmail.com>
2021-05-03 20:45:40 +0200

    WIP

--- build-packages.sh
+++ build-packages.sh
@@ -2,11 +2,15 @@
 
 set -e
 
+gpg --list-keys
+
+exit
 rm -rf build
 mkdir -p build/package
 cp -a ./ build/package || true
 pushd build/package
-fakeroot debian/rules binary
+# fakeroot debian/rules binary
+#dpkg-buildpackage -b
 popd
 
 ls -l build/
