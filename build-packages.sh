96dbb26834984f70c6d355d2457f8872f79670cd
Pawel Soja <kernel32.pl@gmail.com>
2021-05-03 21:32:04 +0200

    WIP

--- build-packages.sh
+++ build-packages.sh
@@ -4,6 +4,8 @@ set -e
 
 gpg --list-keys
 
+echo "KEY $DEB_SIGN_KEYID"
+
 rm -rf build
 mkdir -p build/package
 cp -a ./ build/package || true
@@ -11,5 +13,5 @@ pushd build/package
 # fakeroot debian/rules binary
 dpkg-buildpackage -b
 popd
-
+rm -rf build/package
 ls -l build/
